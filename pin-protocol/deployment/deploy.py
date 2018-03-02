bc2509a00bede31bdd549776ad280b2fbcb84c29
Horacio <horacio.gomez@coinfabrik.com>
2018-03-02 14:36:45 -0300

    scripts for interacting with PinProtocol

--- pin-protocol/deployment/deploy.py
+++ pin-protocol/deployment/deploy.py
@@ -1,24 +1,42 @@
-#!/usr/bin/python3 -i
+#!/home/horacio/web3py-env/bin/python3 -i
+
+import sys
+print(sys.version)
 from web3 import Web3, HTTPProvider
 import json
+#import unlock
+import os, errno
+import time
+from datetime import datetime
 
-web3 = Web3(HTTPProvider('http://localhost:8545'))
+import rlp
+from eth_utils import keccak, to_checksum_address
 
+web3 = Web3(HTTPProvider('http://localhost:7999'))
 eth = web3.eth
+accounts = eth.accounts
+address_log_path = "./address_log"
 
 contract_name = "PinProtocolInvestment"
 
-with open("build/" + contract_name + ".abi") as contract_abi_file:
+# Get contract ABI
+with open("./build/" + contract_name + ".abi") as contract_abi_file:
   contract_abi = json.load(contract_abi_file)
 
-#Comes as string without '0x'
-contract_bin = open("build/" + contract_name + ".bin").read()
-contract = eth.contract(contract_abi, bytecode=("0x" + contract_bin))
+# Get contract Bytecode
+with open("./build/" + contract_name + ".bin") as contract_bin_file:
+  contract_bytecode = '0x' + contract_bin_file.read()
 
-def trans():
-  return {"from": "0x54d9249c776c56520a62faecb87a00e105e8c9dc", "gas": 500000, "gasPrice": 3000000000}
+def generate_contract_address(address, nonce):
+  return to_checksum_address('0x' + keccak(rlp.encode([bytes(bytearray.fromhex(address[2:])), nonce]))[-20:].hex())
 
-dHash = contract.deploy(transaction=trans())
+deployer_nonce = web3.eth.getTransactionCount(accounts[0])
+contract_address = generate_contract_address(accounts[0], deployer_nonce)
+
+# contract instance creation
+contract = web3.eth.contract(address=contract_address, abi=contract_abi, bytecode=contract_bytecode)
+
+deployment_tx = {"from": eth.accounts[0], "gas": 50000000, "gasPrice": 20000000000}
 
 def receipt(tx_hash):
   return eth.getTransactionReceipt(tx_hash)
@@ -26,12 +44,15 @@ def receipt(tx_hash):
 def status(tx_hash):
   print("Status:", eth.getTransactionReceipt(tx_hash)["status"])
 
+d_hash = contract.deploy(transaction=deployment_tx)
+# d_hash = contract.constructor.transact(deployment_tx)
+
 def gas(tx_hash):
   return eth.getTransactionReceipt(tx_hash)["gasUsed"]
 
 def addAddress():
-  status(dHash)
-  contract.address = receipt(dHash).contractAddress
+  status(d_hash)
+  contract.address = receipt(d_hash).contractAddress
   print(contract_name, contract.address)
   
 def partners():
@@ -42,5 +63,4 @@ def partners():
   return partners
 
 def set_transfer_gas(gas_amount):
-  contract.transact(trans()).set_transfer_gas(gas_amount)
-
+  contract.transact(tx()).set_transfer_gas(gas_amount)
