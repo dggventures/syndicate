456dc4084dd5acc95200fafe710f1240bf1cd572
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-03-09 11:00:16 -0300

    Updated folder arrangement

--- syndicate/contracts/Syndicatev2.sol
+++ syndicate/contracts/Syndicatev2.sol
@@ -38,12 +38,12 @@ contract Ownable {
  * Based on https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
  */
 interface EIP20Token {
-  function totalSupply() external view returns (uint256);
-  function balanceOf(address who) external view returns (uint256);
-  function transfer(address to, uint256 value) external returns (bool success);
-  function transferFrom(address from, address to, uint256 value) external returns (bool success);
-  function approve(address spender, uint256 value) external returns (bool success);
-  function allowance(address owner, address spender) external view returns (uint256 remaining);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool success);
+  function transferFrom(address from, address to, uint256 value) public returns (bool success);
+  function approve(address spender, uint256 value) public returns (bool success);
+  function allowance(address owner, address spender) public view returns (uint256 remaining);
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
