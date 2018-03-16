7bb55b5c509e070345898dcb4ef56340447d5dd8
Horacio <horacio.gomez@coinfabrik.com>
2018-03-16 15:59:50 -0300

    RenderToken Investment 1 contract and interfacing script

--- render-token-investment1/deployment/interfacing.py
+++ render-token-investment1/deployment/interfacing.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 print(sys.version)
 from web3 import Web3, HTTPProvider
 
+web3 = Web3(HTTPProvider('http://localhost:8545'))
+
 eth = web3.eth
 
-web3 = Web3(HTTPProvider('http://localhost:8545'))
+contract_name = "RenderTokenInvestment1"
 
 with open("./build/" + contract_name + ".abi") as contract_abi_file:
   contract_abi = json.load(contract_abi_file)
 
-with open("./build" + contract_name + ".bin") as contract_bin_file:
+with open("./build/" + contract_name + ".bin") as contract_bin_file:
   contract_bin = "0x" + contract_bin_file.read()
-
-
