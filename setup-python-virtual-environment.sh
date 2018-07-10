e309dc37ce0cc4a877e8932fa61c385e6516d346
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-07-10 16:23:06 -0300

    Add virtualenv setup script

--- setup-python-virtual-environment.sh
+++ setup-python-virtual-environment.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-python3 -m venv py-venv
-source py-venv/bin/activate
+virtualenv -p python3 ./py-venv
+source ./py-venv/bin/activate
 pip install --upgrade pip setuptools
 pip install --upgrade pytest
 pip install --upgrade web3 eth_utils
