db79bca6c0c9711080064e815c57c958904aaa1a
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-07-10 15:48:13 -0300

    Now using venv library

--- setup-python-virtual-environment.sh
+++ setup-python-virtual-environment.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-virtualenv -p python3 ./py-venv
-source ./py-venv/bin/activate
+python3 -m venv py-venv
+source py-venv/bin/activate
 pip install --upgrade pip setuptools
-pip install --upgrade web3
+pip install --upgrade pytest
+pip install --upgrade web3 eth_utils
