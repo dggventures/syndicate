f4e4690c04b87ca8cb80596be8de1811f9ab06bc
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-07-10 16:03:34 -0300

    Added new utilities

---
+++ syndicate/deployment/cli_args.py
@@ -0,0 +1,10 @@
+import argparse
+
+def parse_args(args):
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-n", "--network", default="poanet", help="Enter network, defaults to poanet")
+  parser.add_argument("-p", "--provider", default="http", help="Enter provider, defaults to http")
+  parser.add_argument("-t", "--test", action="store_true", help="Testing mode")
+  parser.add_argument("-a", "--address", help="Enter address to look for log file")
+  parser.add_argument("-d", "--deployment_name", help="Enter deployment name to look for log file")
+  return parser.parse_known_args(args)
---
+++ syndicate/deployment/web3_interface.py
@@ -0,0 +1,27 @@
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from cli_args import parse_args
+import json
+import sys
+
+
+class Web3Interface:
+
+  def __init__(self):
+    provider_param = None
+    args, unknown = parse_args(sys.argv[1:])
+    with open("../../../deployment/networks.json") as networks_file:
+      networks = json.load(networks_file)
+    if args.provider == "ipc":
+      provider_param = networks[args.network][args.provider]["path"]
+      self.w3 = Web3(Web3.IPCProvider(provider_param))
+    elif args.provider == "ws":
+      provider_param = args.provider + "://" + networks[args.network][args.provider]["host"] + ":" + str(networks[args.network][args.provider]["port"])
+      self.w3 = Web3(Web3.WebsocketProvider(provider_param))
+    elif args.provider == "http":
+      provider_param = args.provider + "://" + networks[args.network][args.provider]["host"] + ":" + str(networks[args.network][args.provider]["port"])
+      self.w3 = Web3(Web3.HTTPProvider(provider_param))
+    else:
+      raise ValueError("Provider not within the admitted options.")
+    if (args.network == "poanet"):
+      self.w3.middleware_stack.inject(geth_poa_middleware, layer=0)
